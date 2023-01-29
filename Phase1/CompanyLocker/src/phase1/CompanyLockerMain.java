7518eddc358375122bb37d888ef4f34358080357
Luis <luisadas1@hotmail.com>
2023-01-29 20:18:27 +0000

    Final Project

---
+++ Phase1/CompanyLocker/src/phase1/CompanyLockerMain.java
@@ -0,0 +1,13 @@
+package phase1;
+
+public class CompanyLockerMain {
+
+	public static void main(String[] args) {
+		// Good Practice for the main class to be simple so I only call a Menu Object
+		Menu obj = new Menu();
+		obj.menu();
+
+	}
+
+
+}
